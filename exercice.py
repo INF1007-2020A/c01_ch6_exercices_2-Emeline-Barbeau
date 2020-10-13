111db8858ebd871aa4f732ea4682ebad78fbf299
Emeline Barbeau <emeline.barbeau@polymtl.ca>
2020-10-13 16:14:12 -0400

    Fini + Commentaires fait

--- exercice.py
+++ exercice.py
@@ -7,36 +7,58 @@ from matplotlib.colors import cnames
 
 def list_to_dict(some_list: list) -> dict:
     # TODO: Transformer la liste en dictionnaire, les éléments de la liste deviennent les clés et leur index deviennent les valeurs
-    return {}
+    
+    some_dict = {}
+    index = 0
 
+    for item in some_list:  # Remplit un dictionnaire avec les infos d'un tableau
+        some_dict[item] = index
+        index += 1
+    
+    return some_dict
 
 def color_name_to_hex(colors: list) -> list:
     # TODO: Trouver la valeur hex de chaque couleur dans la liste et créer une liste de tupple où le premier élément est le nom de la couleur et le deuxième est la valeur hex
-    return []
+    
+    result = [(color, cnames[color]) for color in colors]
+    return result
 
 
 def odd_integer_for_loop(end: int) -> list:
-    return []
+    odd_int = [] 
+    for num in range(1, end, 2):  # Met dans une liste uniquement les nbrs impaires
+        odd_int.append(num)
+
+    return odd_int
 
 
 def odd_integer_list_comprehension(end: int) -> list:
-    return []
+    odd_int = [num for num in range(1, end, 2)] 
+    return odd_int
 
 
 def loop_traversal(integers: list) -> None:
-    pass
+    for index in range(len(integers)):
+        print(f"{index}: {integers[index]}")
+    
+
+def word_dict_for_loop(words: list) -> dict:
+    word_dict = {}
+
+    for word in words:
+        word_dict[word[0].upper()] = word  # Met la 1ère lettre en majuscule en key du dictionnaire
+
+    return word_dict
 
 
-def word_dict_for_loop() -> dict:
-    return {}
-
-
-def word_dict_comprehension() -> dict:
-    return {}
+def word_dict_comprehension(words: list) -> dict:
+    word_dict = {word[0].upper(): word for word in words}
+    return word_dict
 
 
 def dictionary_traversal(words: dict) -> None:
-    pass
+    for key in sorted(words.keys()):
+        print(f"{key}: {words[key]}") 
 
 
 def main() -> None:
@@ -64,7 +86,7 @@ def main() -> None:
 
     print(f"Les 2 dictionnaires sont-ils identiques? {words_for == words_comprehension}")
     print(f"Parcours d'un des 2 dictionnaires...")
-    loop_traversal(words_comprehension)
+    dictionary_traversal(words_comprehension)
 
 
 if __name__ == '__main__':
